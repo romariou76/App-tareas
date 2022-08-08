fb068eb1c7209c0ef36989fe756f279b34638b3d
romariou76 <illeniumr@gmail.com>
2022-08-07 19:12:39 -0500

    He integrado una app de tareas para estilizarlo y acoplarlo  en la estructura de la app principal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Drawer from "./components/Drawer.jsx";
-import Main from './components/header.jsx';
 import Page from './components/main.jsx';
 import './index.css'
 
